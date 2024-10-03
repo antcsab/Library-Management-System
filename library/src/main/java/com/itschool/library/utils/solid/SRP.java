d6c56a1a2b93b0eefcecd638ad5b9b3061a35f9f
Antal Csaba <antcsab@gmail.com>
2024-10-03 21:28:07 +0300

    LibraryManagementSystem - SOLID

---
+++ library/src/main/java/com/itschool/library/utils/solid/SRP.java
@@ -0,0 +1,30 @@
+package com.itschool.library.utils.solid;
+
+public class SRP {
+}
+
+//Before: violating SRP
+
+class UserService {
+
+    void addUser() {
+    }
+
+    void sendEmail() {
+    }
+}
+
+
+// After: following SRP
+
+class UserService2 {
+
+    void addUser() {
+    }
+}
+
+class EmailService {
+
+    void sendEmail() {
+    }
+}
